48dc6bf02aed9a69ab96fe9dd01387d9845e4afa
Erik Sierra <120680439+ErikSierra@users.noreply.github.com>
2024-05-20 12:23:42 -0400

    Update cmdCommands.bash

--- cmdCommands.bash
+++ cmdCommands.bash
@@ -1,3 +1,13 @@
+## Commands to run in cmd:
+
+python -m venv venv
+source venv/bin/activate   # On Windows, use `venv\Scripts\activate`
+
+-------------------------------------------------
+
+pip install -r requirements.txt
+
+--------------------------------------------------
 set CROWDSTRIKE_CLIENT_ID=your_client_id_here
 set CROWDSTRIKE_CLIENT_SECRET=your_client_secret_here
 python test_crowdstrike_connection.py
