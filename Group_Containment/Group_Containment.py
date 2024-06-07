4aef2e043c82c9c9e1f8d3078b1e906bb78c62db
Erik Sierra <erik.sierra77@gmail.com>
2024-06-07 15:10:04 -0400

    Update

--- Group_Containment/Group_Containment.py
+++ Group_Containment/Group_Containment.py
@@ -1,127 +1,62 @@
-import os
-import falconpy
 import yaml
-import json
-import sys
-from falconpy import Hosts, RealTimeResponse, APIError
-from colorama import init, Fore, Back, Style
-import subprocess
-import sys
+import argparse
+from falconpy import Hosts
 
+# Setup our argument parser
+parser = argparse.ArgumentParser("Script that leverages Falcon API to (un)contain hosts")
+parser.add_argument('-c', '--creds_file', dest='creds_file', help='Path to creds yaml file', required=True)
+parser.add_argument('-g', '--group_name', dest='group_name', help='Name of the group to quarantine', required=True)
+parser.add_argument('-l', '--lift', dest='lift_containment', action="store_true", help='Lift containment', default=False)
 
-init()
+# Parse our ingested arguments
+args = parser.parse_args()
 
-# Constants
-CONFIG_FILE = 'config.yaml'
+# Default action is to quarantine
+if args.lift_containment:
+    action = "lift_containment"
+else:
+    action = "contain"
 
+# Use the credentials file provided
+creds_file = args.creds_file
 
-# Function to load configuration
-def load_config(file_path):
-    if not os.path.isfile(file_path):
-        print(f"Error: Configuration file '{file_path}' not found.")
-        sys.exit(1)
+# Load the contents of the creds file using the PyYAML parser library
+with open(creds_file, 'r') as file_creds:
+    creds = yaml.safe_load(file_creds)
 
-    try:
-        with open(file_path, 'r') as f:
-            config = yaml.safe_load(f)
-            return config
-    except yaml.YAMLError as e:
-        print(f"Error reading configuration file: {e}")
-        sys.exit(1)
+# Create an instance of our OAuth2 authorization class using our ingested creds
+falcon = Hosts(client_id=creds["falcon_client_id"], client_secret=creds["falcon_client_secret"])
 
+# Query the Hosts API for hosts that match our filter pattern
+response = falcon.query_devices_by_filter(filter=f"group_name:'{args.group_name}'", limit=10)
 
-# Function to test the connection to the CrowdStrike API
-def test_crowdstrike_connection(config):
-    if not config:
-        return
+if response["status_code"] != 200:
+    raise SystemExit("Unable to retrieve list of devices that match the group name specified.")
 
-    try:
-        # API credentials
-        client_id = config['api']['client_id']
-        client_secret = config['api']['client_secret']
-    except KeyError as e:
-        print(f"Missing API credential in configuration file: {e}")
-        sys.exit(1)
+# Retrieve the list of IDs returned
+contain_ids = response["body"]["resources"]
 
-    # Connect to the CrowdStrike API
-    try:
-        falcon_hosts = Hosts(client_id=client_id, client_secret=client_secret)
-        # Perform a simple request to verify the connection
-        response = falcon_hosts.query_devices_by_filter(limit=1)
-        if response["status_code"] == 200:
-            print(Fore.BLUE + "Successfully connected to the CrowdStrike API." + Style.RESET_ALL)
-            # Debug: Print the response from the API
-            # print(f"API Response: {json.dumps(response, indent=4)}")
-        elif response["status_code"] == 401:
-            print(Fore.RED + "Unauthorized: Please check your API credentials in the .yaml file." + Style.RESET_ALL)
-            sys.exit(1)
-            # Debug: Print more details for troubleshooting
-            # print(Fore.RED + f"Response details: {json.dumps(response, indent=4)}" + Style.RESET_ALL)
-        else:
-            print(Fore.RED + f"Failed to connect to the CrowdStrike API. Status code: {response['status_code']}" +
-                  Style.RESET_ALL)
-            sys.exit(1)
-            # Debug: Print more details for troubleshooting
-            # print(f"Response details: {json.dumps(response, indent=4)}")
-    except APIError as e:
-        print(Fore.RED + f"APIError during authentication: {e.message}" + Style.RESET_ALL)
-        sys.exit(1)
-    except Exception as e:
-        print(Fore.RED + f"Error during API connection: {e}" + Style.RESET_ALL)
-        sys.exit(1)
+if not contain_ids:
+    # No hosts were found, exit out
+    raise SystemExit(f"[-] Could not find group name: {args.group_name} - Please verify proper case")
 
+# Provide a status update to the terminal
+if action == "contain":
+    print(f"\n[+] Containing group: {args.group_name}\n")
+    blurb = "contained"
+else:
+    print(f"\n[+] Lifting Containment for group: {args.group_name}\n")
+    blurb = "released"
 
-# Define the ID of the Host Group to be contained
-host_group_id = "ac71d7e8c876456eb10424ca96f2049d"
+# Perform the requested action
+response = falcon.perform_action(ids=contain_ids, action_name=action)
 
-# Define the duration of the containment in seconds
-containment_duration = 3600
-
-# Initiate containment for the Host Group
-def contain_host_by_id(falcon_hosts, host_id):
-    try:
-        response = falcon_hosts.perform_action(action_name="contain", ids=[host_id])
-        return response
-    except APIError as e:
-        print(Fore.RED + f"APIError containing host ID {host_id}: {e.message}" + Style.RESET_ALL)
-        sys.exit(1)
-    except Exception as e:
-        print(Fore.RED + f"Error containing host ID {host_id}: {e}" + Style.RESET_ALL)
-        sys.exit(1)
-
-
-# Load configuration file
-config = load_config(CONFIG_FILE)
-
-# Test connection to CrowdStrike API
-test_crowdstrike_connection(config)
-
-# Connect to the CrowdStrike Hosts API
-falcon_hosts = Hosts(access_token=config['api']['access_token'])
-
-# Initiate containment for all Hosts in the Host Group
-try:
-    # Query the Host Group to get a list of Host IDs
-    print("Querying the Host Group for Host IDs...")
-    query = falcon_hosts.query_devices(filter=f"falcon_host_group_id:'{host_group_id}'")
-    if query["status_code"] != 200:
-        print(Fore.RED + f"Error querying Hosts: {query['status_code']} {query['body']}" + Style.RESET_ALL)
-        sys.exit(1)
-    hosts = [device['id'] for device in query['body']['resources']]
-    print(Fore.BLUE + f"Found {len(hosts)} Hosts in the Host Group." + Style.RESET_ALL)
-
-    # Initiate containment for each Host
-    for host_id in hosts:
-        print(f"Initiating containment for Host ID: {host_id}")
-        response = contain_host_by_id(falcon_hosts, host_id)
-        if response["status_code"] == 200:
-            print(Fore.GREEN + f"Containment successfully initiated for Host ID: {host_id}" + Style.RESET_ALL)
-        else:
-            print(Fore.RED + f"Failed to initiate containment for Host ID: {host_id}. Status code: {response['status_code']}" + Style.RESET_ALL)
-
-except APIError as e:
-    print(Fore.RED + f"APIError when querying Host Group: {e.message}" + Style.RESET_ALL)
-    sys.exit(1)
-except Exception as e:
-    print(Fore.RED + f"Error when querying Host Group: {e}" + Style.RESET_ALL)
-    sys.exit(1)
+if response["status_code"] == 202:
+    for contained in response["body"]["resources"]:
+        print(f"{contained['id']} has been {blurb}.")
+else:
+    error_list = response["body"]["errors"]
+    for err in error_list:
+        ecode = err["code"]
+        emsg = err["message"]
+        print(f"[{ecode}] {emsg}")
