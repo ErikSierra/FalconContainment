585e263ba28df79064d1a84a1cce341dfadb64da
Erik Sierra <erik.sierra77@gmail.com>
2024-06-10 12:59:07 -0400

    Update

--- FalconTests/GroupHosts_Test.py
+++ FalconTests/GroupHosts_Test.py
@@ -1,14 +1,12 @@
 from falconpy import HostGroup
+import json
 
-# Do not hardcode API credentials!
-falcon = HostGroup(client_id=CLIENT_ID,
-                   client_secret=CLIENT_SECRET
-                   )
+host_group = HostGroup(client_id="<YOUR CLIENT ID>", client_secret="<YOUR CLIENT SECRET>")
 
-response = falcon.query_combined_group_members(id="ac71d7e8c876456eb10424ca96f2049d",
-                                               filter="string",
-                                               offset=0,
-                                               limit=100,
-                                               sort="string"
-                                               )
-print(response)
+group_id = "ac71d7e8c876456eb10424ca96f2049d"
+
+filter_str = "os:'Windows' and hostname:*.local and last_seen:<now>"
+
+response = host_group.query_combined_group_members(id=group_id, filter=filter_str)
+
+print(json.dumps(response, indent=4))
