146ac48721e1e2d0f0fe4518dc179fb9e3cf6b57
Erik Sierra <erik.sierra77@gmail.com>
2024-06-13 15:05:36 -0400

    Update

--- FalconTests/GetMembersTest.py
+++ FalconTests/GetMembersTest.py
@@ -1,18 +1,14 @@
 from falconpy import HostGroup
 
 # Do not hardcode API credentials!
-falcon = HostGroup(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+falcon = HostGroup(client_id=CLIENT_ID,
+                   client_secret=CLIENT_SECRET
+                   )
 
-response = falcon.query_combined_group_members(
-    id="ac71d7e8c876456eb10424ca96f2049d",
-    offset=0,
-    limit=5000,
-    sort="hostname"
-)
-
-if response.get("status_code") != 200:
-    print(f"Error: {response.get('error_message')}")
-else:
-    # Extract the hostname from each member object
-    hostnames = [member.get("hostname") for member in response.get("resources", [])]
-    print("Hostnames:", hostnames)
+response = falcon.query_combined_group_members(id="ac71d7e8c876456eb10424ca96f2049d",
+                                               filter="hostname",
+                                               offset=0,
+                                               limit=5000,
+                                               sort="hostname"
+                                               )
+print(response)
