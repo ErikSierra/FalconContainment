6e13700193baa4ea385f42e65617ee66b58e0627
Erik Sierra <erik.sierra77@gmail.com>
2024-06-13 16:31:41 -0400

    Update

--- FalconTests/GetMembersTest.py
+++ FalconTests/GetMembersTest.py
@@ -1,16 +1,15 @@
-from falconpy import Hosts
 
-# Enable pythonic responses when you construct an instance of the class
-hosts = Hosts(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, pythonic=True)
 
-# Define the group name to filter by
-group_name = 'Workflow Testing'
+from falconpy.hosts import Hosts
 
-# Construct the filter
-filter = f"falcon_sandbox_id:{hosts.creds.sandbox_id} AND group_name:'{group_name}'"
+# Replace GROUP_ID with the ID of the group you want to query
+GROUP_ID = "my-group-id"
 
-# Query devices by the filter and loop through the results
-result = hosts.query_devices_by_filter_scroll(filter=filter)
-if not result.raw:
-    for device_id in result:
-        print(device_id)
+# Create a Hosts instance and authenticate
+falcon = Hosts(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+
+# Query devices by group ID
+devices = falcon.query_devices_by_filter(filter=f"group_id:'{GROUP_ID}'", sort="hostname.asc")
+
+# Print the results
+print(devices)
