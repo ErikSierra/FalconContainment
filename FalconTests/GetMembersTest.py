c35537a930e497ef136dc0d5be49f33e0e6cf134
Erik Sierra <erik.sierra77@gmail.com>
2024-06-13 10:10:35 -0400

    Update

---
+++ FalconTests/GetMembersTest.py
@@ -0,0 +1,14 @@
+from falconpy import HostGroup
+
+# Do not hardcode API credentials!
+falcon = HostGroup(client_id=CLIENT_ID,
+                   client_secret=CLIENT_SECRET
+                   )
+
+response = falcon.query_combined_group_members(id="ac71d7e8c876456eb10424ca96f2049d",
+                                               filter="string",
+                                               offset=0,
+                                               limit=5000,
+                                               sort="string"
+                                               )
+print(response)
