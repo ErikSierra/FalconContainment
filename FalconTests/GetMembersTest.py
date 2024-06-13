07cc96ac6c1c094619367df43741c8130910fc6a
Erik Sierra <erik.sierra77@gmail.com>
2024-06-13 15:38:43 -0400

    Update

--- FalconTests/GetMembersTest.py
+++ FalconTests/GetMembersTest.py
@@ -1,13 +1,15 @@
-from falconpy import HostGroup
+# Store the response data in a variable
+response_data = falcon.query_combined_group_members(id="ac71d7e8c876456eb10424ca96f2049d",
+                                                     offset=0,
+                                                     limit=5000,
+                                                     sort="hostname"
+                                                     )
 
-# Do not hardcode API credentials!
-falcon = HostGroup(client_id=CLIENT_ID,
-                   client_secret=CLIENT_SECRET
-                   )
-
-response = falcon.query_combined_group_members(id="ac71d7e8c876456eb10424ca96f2049d",
-                                               offset=0,
-                                               limit=5000,
-                                               sort="hostname"
-                                               )
-print(response)
+# Loop through the response data and print the details for each host
+for host in response_data["resources"]:
+    print("HOST DETAILS:")
+    print(f"Hostname: {host.get('hostname')}")
+    print(f"IP Address: {host.get('local_ip')}")
+    print(f"OS: {host.get('os_version')}")
+    print(f"Last Seen: {host.get('last_seen')}")
+    print(f"\n")
