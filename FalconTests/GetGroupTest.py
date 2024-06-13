434318ddc6d5fbc614d12d96ab5490eeb98b1359
Erik Sierra <erik.sierra77@gmail.com>
2024-06-13 10:49:23 -0400

    Update

--- FalconTests/GetGroupTest.py
+++ FalconTests/GetGroupTest.py
@@ -5,7 +5,7 @@ falcon = HostGroup(client_id=CLIENT_ID,
                    client_secret=CLIENT_SECRET
                    )
 
-response = falcon.query_combined_host_groups(filter="string",  # Query to filter host groups
+response = falcon.query_combined_host_groups(filter="",  # Query to filter host groups
                                              offset=0,
                                              limit=5000,
                                              sort="string"
