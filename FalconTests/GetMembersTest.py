945deb82c65fb851bddd0cefe246e5300cdcc27a
Erik Sierra <erik.sierra77@gmail.com>
2024-06-13 15:31:30 -0400

    Update

--- FalconTests/GetMembersTest.py
+++ FalconTests/GetMembersTest.py
@@ -6,7 +6,8 @@ falcon = HostGroup(client_id=CLIENT_ID,
                    )
 
 response = falcon.query_combined_group_members(id="ac71d7e8c876456eb10424ca96f2049d",
-                                               select=["hostname"]
+                                               offset=0,
+                                               limit=5000,
+                                               sort="hostname"
                                                )
-for member in response["resources"]:
-    print(member["hostname"])
+print(response)
