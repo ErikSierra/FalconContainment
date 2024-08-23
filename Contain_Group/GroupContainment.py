a8ec093f7feacf5569e44b2956ab88b8633660bc
Rajiv Kaushik <zayeemzaki45@gmail.com>
2024-08-23 17:49:12 -0400

    updated contain in contain_group

--- Contain_Group/GroupContainment.py
+++ Contain_Group/GroupContainment.py
@@ -2,7 +2,7 @@ import json
 import sys
 from falconpy import Hosts, HostGroup, APIError
 from colorama import init, Fore, Style
-from LoadConfig import load_config # loads config.yaml
+from LoadConfig import load_config
 import pandas as pd
 init()
 from datetime import datetime
@@ -14,49 +14,6 @@ def log_containment_action(hostname, host_id, action, status):
     with open("group_containment_log.txt", "a") as log_file:
         log_file.write(log_message)
 
-# Function to test the connection to the CrowdStrike API
-def test_crowdstrike_connection(client_id, client_secret):
-    try:
-        falcon_hosts = Hosts(client_id=client_id, client_secret=client_secret)
-        response = falcon_hosts.query_devices_by_filter(limit=1)
-        if response["status_code"] == 200:
-            print(Fore.BLUE + "Successfully connected to the CrowdStrike API \nGroups found in your Crowdstrike environment:"
-                   + Style.RESET_ALL)
-        elif response["status_code"] == 401:
-            print(Fore.RED + "Unauthorized: Please check your API credentials in the .yaml file." + Style.RESET_ALL)
-            sys.exit(1)
-        else:
-            print(Fore.RED + f"Failed to connect to the CrowdStrike API. Status code: {response['status_code']}" +
-                  Style.RESET_ALL)
-            sys.exit(1)
-    except APIError as e:
-        print(Fore.RED + f"APIError during authentication: {e.message}" + Style.RESET_ALL)
-        sys.exit(1)
-    except Exception as e:
-        print(Fore.RED + f"Error during API connection: {e}" + Style.RESET_ALL)
-        sys.exit(1)
-
-# Function to contain a host by its ID
-def contain_host_by_id(falcon_hosts, host_id):
-    try:
-        response = falcon_hosts.perform_action(action_name="contain", ids=[host_id])
-        return response
-    except APIError as e:
-        print(Fore.RED + f"APIError containing host ID {host_id}: {e.message}" + Style.RESET_ALL)
-    except Exception as e:
-        print(Fore.RED + f"Error containing host ID {host_id}: {e}" + Style.RESET_ALL)
-        return None
-
-# Function to un-contain a host by its ID
-def uncontain_host_by_id(falcon_hosts, host_id):
-    try:
-        response = falcon_hosts.perform_action(action_name="lift_containment", ids=[host_id])
-        return response
-    except APIError as e:
-        print(Fore.RED + f"APIError un-containing host ID {host_id}: {e.message}" + Style.RESET_ALL)
-    except Exception as e:
-        print(Fore.RED + f"Error un-containing host ID {host_id}: {e}" + Style.RESET_ALL)
-        return None
 
 # Function to get host groups
 def get_host_groups(client_id, client_secret):
@@ -80,7 +37,7 @@ def get_host_groups(client_id, client_secret):
     else:
         print(f"Error: {response['body']['errors'][0]['message']}")
         return []
-
+    
 # Function to get group members
 def get_group_members(client_id, client_secret, group_id):
     falcon = HostGroup(client_id=client_id, client_secret=client_secret)
@@ -102,6 +59,43 @@ def get_group_members(client_id, client_secret, group_id):
     else:
         print(f"Error: {response['body']['errors'][0]['message']}")
         return []
+    
+
+# Function to contain a host by its ID
+def contain_host_by_id(falcon_hosts, host_id):
+    try:
+        response = falcon_hosts.perform_action(action_name="contain", ids=[host_id])
+        return response
+    except APIError as e:
+        print(Fore.RED + f"APIError containing host ID {host_id}: {e.message}" + Style.RESET_ALL)
+    except Exception as e:
+        print(Fore.RED + f"Error containing host ID {host_id}: {e}" + Style.RESET_ALL)
+        return None
+
+# Function to un-contain a host by its ID
+def uncontain_host_by_id(falcon_hosts, host_id):
+    try:
+        response = falcon_hosts.perform_action(action_name="lift_containment", ids=[host_id])
+        return response
+    except APIError as e:
+        print(Fore.RED + f"APIError un-containing host ID {host_id}: {e.message}" + Style.RESET_ALL)
+    except Exception as e:
+        print(Fore.RED + f"Error un-containing host ID {host_id}: {e}" + Style.RESET_ALL)
+        return None
+    
+
+# Function to check containment status
+def containment_status(hosts, client_id, client_secret):
+    falcon_hosts = Hosts(client_id=client_id, client_secret=client_secret)
+    
+    for host_id in hosts:
+        result = falcon_hosts.get_device_details(ids=host_id)
+        if result["status_code"] == 200:
+            status = result["body"]["resources"][0]["status"]
+            hostname = result["body"]["resources"][0]["hostname"]
+            print(f"Hostname: {hostname}, ID: {host_id} ==> Status: {status}")
+        else:
+            print(result["body"]["errors"])
 
 # Function to contain hosts
 def contain_hosts(hosts, client_id, client_secret):
@@ -111,30 +105,40 @@ def contain_hosts(hosts, client_id, client_secret):
     failed_to_contain_hosts = []
 
     for host_id in hosts:
-        containment_response = contain_host_by_id(falcon_hosts, host_id)
-        time.sleep(60)
+        contain_host_by_id(falcon_hosts, host_id)
 
-        if containment_response:
-            if containment_response["status_code"] == 200 and not containment_response["body"].get("errors"):
-                successfully_contained_hosts.append(host_id)
-                hostname = containment_response["body"]["resources"][0]["hostname"]
-                log_containment_action(hostname, host_id, "contain", "success")
-                print(Fore.BLUE + f"Successfully contained {hostname} ({host_id})" + Style.RESET_ALL)
-            elif containment_response["status_code"] == 202 and not containment_response["body"].get("errors"):
-                pending_contained_hosts.append(host_id)
-                hostname = containment_response["body"]["resources"][0]["hostname"]
-                log_containment_action(hostname, host_id, "contain", "pending")
+    time.delay(60)
+
+    for host_id in hosts:
+        result = falcon_hosts.get_device_details(ids=host_id)
+
+        if result["status_code"] == 200:
+            status = result["body"]["resources"][0]["status"]
+            hostname = result["body"]["resources"][0]["hostname"]
+            if status == "contained":
+                successfully_contained_hosts.append(hostname, host_id)
+            elif status == "normal":
+                failed_to_contain_hosts.append(hostname, host_id)
             else:
-                failed_to_contain_hosts.append(host_id)
-                hostname = containment_response["body"]["resources"][0]["hostname"]
-                log_containment_action(hostname, host_id, "contain", "failed")
-                print(Fore.RED + f"Failed to contain {hostname} ({host_id}): {json.dumps(containment_response, indent=4)}" + Style.RESET_ALL)
+                pending_contained_hosts.append(hostname, host_id)
         else:
-            failed_to_contain_hosts.append(host_id)
-            log_containment_action("Unknown", host_id, "contain", "no response")
-            print(Fore.RED + f"Failed to contain {host_id}: No response from containment request" + Style.RESET_ALL)
+            print(result["body"]["errors"])
 
-    print_summary(successfully_contained_hosts, pending_contained_hosts, failed_to_contain_hosts)
+    print("SuccessFully Contained: \n")
+    for name, id in successfully_contained_hosts:
+        print("Host name: ", name, " Host id: ", id)
+        log_containment_action(hostname, host_id, "contain", "contained")
+ 
+    print("Pending containment: \n")
+    for name, id in pending_contained_hosts:
+        print("Host name: ", name, " Host id: ", id)
+        log_containment_action(hostname, host_id, "contain", "pending")
+
+    print("Failed containment: \n")
+    for name, id in failed_to_contain_hosts:
+        print("Host name: ", name, " Host id: ", id)
+        log_containment_action(hostname, host_id, "contain", "normal")
+         
 
 
 # Function to lift containment for hosts
@@ -168,67 +172,38 @@ def lift_containment(hosts, client_id, client_secret):
             log_containment_action("Unknown", host_id, "lift", "no response")
             print(Fore.RED + f"Failed to un-contain {host_id}: No response from un-containment request" + Style.RESET_ALL)
 
-    print_summary(successfully_uncontained_hosts, pending_uncontained_hosts, failed_to_uncontain_hosts)
 
 
 
-# Function to print summary
-def print_summary(successfully_contained_hosts, pending_contained_hosts, failed_to_contain_hosts):
-    print("\n============================================================================================================")
-    print(Fore.BLUE + "Successfully contained hosts:" + Style.RESET_ALL)
-    for host in successfully_contained_hosts:
-        print(Fore.BLUE + f"- {host}" + Style.RESET_ALL)
-
-    print(Fore.YELLOW + "\nPending containment hosts:" + Style.RESET_ALL)
-    for host in pending_contained_hosts:
-        print(Fore.YELLOW + f"- {host}" + Style.RESET_ALL)
-
-    print(Fore.RED + "\nHosts not contained:" + Style.RESET_ALL)
-    for host in failed_to_contain_hosts:
-        print(Fore.RED + f"- {host}" + Style.RESET_ALL)
-
-
-# Function to check containment status
-def containment_status(hosts, client_id, client_secret):
-    falcon = Hosts(client_id=client_id, client_secret=client_secret)
-    for host in hosts:
-        result = falcon.get_device_details(ids=host)
-        if result["status_code"] == 200:
-            state = result["body"]["resources"][0]["status"]
-            hostname = result["body"]["resources"][0]["hostname"]
-            print(f"Hostname: {hostname}, ID: {host} ==> Status: {state}")
-        else:
-            print(result["body"]["errors"])
 
 
 def main():
-    config = load_config('config.yaml')     # Load the configuration
+    config = load_config('config.yaml')
 
-    CLIENT_ID, SECRET_ID = config['client_id'], config['client_secret']
+    CLIENT_ID = config['client_id']
+    CLIENT_SECRET = config['client_secret']
 
-    test_crowdstrike_connection(CLIENT_ID, SECRET_ID)    # Test the connection to the CrowdStrike API
-
-    groups = get_host_groups(config['client_id'], config['client_secret'])     # Retrieve and display host groups
+    groups = get_host_groups(CLIENT_ID, CLIENT_SECRET)
     if not groups: return  
 
     # User selects a group ID
     selected_group_id = input("Enter your Group ID: ")
 
     # Retrieve and display group members
-    members = get_group_members(config['client_id'], config['client_secret'], selected_group_id)
+    members = get_group_members(CLIENT_ID, CLIENT_SECRET, selected_group_id)
     if not members:
         print("No members found in the selected group.")
         return
 
     while True:
-        # Option to check containment status or lift containment
-        action = input("Do you want to contain the hosts, check status, lift containment, or none? (contain/status/lift/none): ").lower()
+        action = input("Do you want to: \n1. contain the hosts \"contain\" \n2. check status \"status\" \n3. lift containment \"lift\" \n4. none \"none\" \n").lower()
+
         if action == "status":
-            containment_status(members, config['client_id'], config['client_secret'])
+            containment_status(members, CLIENT_ID, CLIENT_SECRET)
         elif action == "contain":
-            contain_hosts(members, config['client_id'], config['client_secret'])
+            contain_hosts(members, CLIENT_ID, CLIENT_SECRET)
         elif action == "lift":
-            lift_containment(members, config['client_id'], config['client_secret'])
+            lift_containment(members, CLIENT_ID, CLIENT_SECRET)
         elif action == "none":
             print("No further action taken.")
             break
